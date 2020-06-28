5a03fbb907569f0259977310c7d14a889a794be2
Jasen Jacobsen <jasen.robert.jacobsen@gmail.com>
2020-06-28 10:33:22 -0700

    Added budget script and ROMS_Tools

---
+++ Dev_ControlVolumeBudget.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 11:22:20 2020
+
+@author: Jasen
+"""
+import os
+os.chdir('/Users/Jasen/Documents/GitHub/ROMS_Budget/')
+
+import ROMS_Tools as RT
+#Locate file
+RomsFile = ''
+
+#coordinates of desired mask
+latbounds = [34, 36]
+lonbounds = [-125, -126]
+
+# Extract Data at mask
+ROMS = RT.ROMS_Load(RomsFile, 'Salt', latbounds, lonbounds)
+
+#Variance budget
+#Terms 1 & 2: define scalar values
+#   Volume average
+
+#   Purturbation (prime)
+
+#Term 3: change in volume integrated purturbation
+#   square prime in each cell
+
+#   multiply each by cell size dx dy dz
+
+#   sum each term over total volume
+
+#   recompute for each time step
+
+#Term 4: internal variance diffusion
+#   Correct for tilted sigma-surface
+
+#   d prime/dx, d prime/dy, d prime/dz
+
+#   d/dx(Kh*d prime/dx), d/dy(Kh* d prime/dy)
+
+#   compute Kv by interpolating to rho point
+
+#   d/dz(Kv*d prime/dz)
+
+#Term 5: advective flux through boundary
+#   offshore face: u*prime^2*cell size in each cell, sum
+
+#   inshore face: u*prime^2*cell size in each cell, sum
+
+#   north face: v*prime^2*cell size in each cell, sum
+
+#   south face: v*prime^2*cell size in each cell, sum
+
+#Term 6: diffusive flux through boundary
+#   offshore face
+
+#   inshore face
+
+#   north face
+
+#   south face
+
---
+++ ROMS_Tools.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 12:29:58 2020
+
+@author: Jasen
+"""
+# ROMS data extraction for CV budget
+import numpy as np
+from netCDF4 import Dataset as dt
+
+def ROMS_Load(RomsFile, VarName, latbounds, lonbounds) :
+    """Loads lat_rho/psi, lon_rho/psi, time, u, ubar, v, vbar, w, and a defined variable \
+    at defined latitude and longitude. Stores in dictionary"""
+    #load nc file   
+    RomsNC = dt(RomsFile, 'r')
+    
+    #locate rho points within bounds 
+    lat_li = np.argmin(np.abs(RomsNC.variables['lat_rho'][:] - latbounds[0]))
+    lat_ui = np.argmin(np.abs(RomsNC.variables['lat_rho'][:] - latbounds[1]))
+    lon_li = np.argmin(np.abs(RomsNC.variables['lon_rho'][:] - lonbounds[0]))
+    lon_ui = np.argmin(np.abs(RomsNC.variables['lon_rho'][:] - latbounds[1]))
+    
+    #locate psi points within bounds
+    latpsi_li = np.argmin(np.abs(RomsNC.variables['lat_psi'][:] - latbounds[0]))
+    latpsi_ui = np.argmin(np.abs(RomsNC.variables['lat_psi'][:] - latbounds[1]))
+    lonpsi_li = np.argmin(np.abs(RomsNC.variables['lon_psi'][:] - lonbounds[0]))
+    lonpsi_ui = np.argmin(np.abs(RomsNC.variables['lon_psi'][:] - latbounds[1]))
+    
+    #subset variables and store in dictionary
+    ROMS = {'lat_rho' : np.array(RomsNC.variables['lat_rho'][lat_li:lat_ui, lon_li:lon_ui], dtype = np.float64), \
+            'lon_rho' : np.array(RomsNC.variables['lon_rho'][ lat_li:lat_ui, lon_li:lon_ui], dtype = np.float64), \
+            'time' : np.array(RomsNC.variables['ocean_time'][:], dtype = np.float64), \
+            'w' : np.array(RomsNC.variables['w'][:, :, lat_li:lat_ui, lon_li:lon_ui], dtype = np.float64), \
+            VarName : np.array(RomsNC.variables[VarName][:, :, lat_li:lat_ui, lon_li:lon_ui], dtype = np.float64), \
+            # psi variables (on edges)
+            'lat_edge': np.array(RomsNC.variables['lat_psi'][latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64), \
+            'lon_edge': np.array(RomsNC.variables['lon_psi'][latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64), \
+            'u' : np.array(RomsNC.variables['u'][:, :, latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64), \
+            'ubar' : np.array(RomsNC.variable['ubar'][:, :, latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64), \
+            'v' : np.array(RomsNC.variables['v'][:, :, latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64), \
+            'vbar' : np.array(RomsNC.variables['vbar'][:, :, latpsi_li:latpsi_ui, lonpsi_li:lonpsi_ui], dtype = np.float64)}
+        
+    return ROMS
+
+def Reynolds_Avg(Var):
+    """takes temporal average at each grid cell and subtracts to get avg and prime terms """
+    # temporal mean
+    Var_Tmean = np.mean(Var, axis = 0)
+    
+    # prime value
+    Var_prime = Var - Var_Tmean
+    
+    ReynAvg = {'bar' : Var_Tmean, 'prime' : Var_prime}
+    
+    return ReynAvg
+
