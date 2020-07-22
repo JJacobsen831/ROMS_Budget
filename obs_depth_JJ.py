3b8caa63ec201a215e18fed567a0caa18142ad2a
Jasen Jacobsen <jasen.robert.jacobsen@gmail.com>
2020-07-22 13:27:45 -0700

    added time dependent depth subroutine

--- obs_depth_JJ.py
+++ obs_depth_JJ.py
@@ -164,6 +164,116 @@ def _set_depth(romsfile=None, romsvars=None, point_type=None, h=None, zeta=0):
         
     return z
 
+def _set_depth_T(romsfile=None, romsvars=None, point_type=None, h=None, zeta=0):
+    '''
+    Computes depth of roms grid taking time dependent sea surface height
+    Modelled after matlab function "roms/repository/matlab/utility/set_depth.m"
+    '''
+    if romsvars is None or h is None:
+        romsvars = dict()
+        with nc4.Dataset(romsfile) as nc:
+            for v in ['Vstretching', 'theta_s', 'theta_b', 'hc', 'h']:
+                romsvars[v] = nc.variables[v][:]
+            romsvars['N'] = nc.variables['Cs_r'].size
+            h = nc.variables['h'][:]
+
+    if point_type is None:
+        point_type = 'rho'
+
+    use_w = point_type in [5, 'w']
+
+    s, C = _stretching(romsvars=romsvars, use_w=use_w)
+    
+    
+    if romsvars['Vstretching'] == 1:
+        #Indices to "shift" averaging grid
+        N = romsvars['N']
+        Lp, Mp = romsvars['h'].shape
+        L = Lp - 1
+        M = Mp - 1
+        
+        
+        
+        if point_type in [1, 'rho', 'density']:
+            hr = h
+            zetar = zeta
+            
+            z = np.empty((zetar.shape[0],N+use_w, hr.shape[0], hr.shape[1]))
+            z.fill(np.nan)
+            
+            for k in range(z.shape[0]) : 
+                z0 = (s[k]-C[k])*romsvars['hc'] + C[k]*hr
+                
+                for i in range(zetar.shape[0]) : 
+                    z[i, k, :, :] = z0 + zetar[i, :, :]*(1.0 + z0/hr)
+
+        elif point_type in [2, 'psi', 'streamfunction']:         
+            #average bathymetry and free surface
+            hp = 0.25*(h[0:L, 0:M] + h[1:Lp, 0:M] + \
+                       h[0:L, 1:Mp] + h[1:Lp, 1:Mp])
+            zetap = 0.25*(zeta[:, 0:L, 0:M] + zeta[:, 1:Lp, 0:M] + \
+                          zeta[:, 0:L, 1:Mp] + zeta[:, 1:Lp, 1:Mp])
+            
+            z = np.empty((zetap.shape[0], N+use_w, hp.shape[0], hp.shape[1]))
+            z.fill(np.nan)
+            
+            #compute depth
+            for k in range(z.shape[0]):
+                z0 = (s[k]-C[k])*romsvars['hc'] + C[k]*hp
+                
+                for i in range(zetap.shape[0]) :
+                    z[i, k, :, :] = z0 + zetap[i, :, :]*(1.0 + z0/hp)
+            
+        elif point_type in [3, 'u']:
+            #averaging
+            hu = 0.5*(h[0:L, 0:Mp] + h[1:Lp, 0:Mp])
+            zetau = 0.5*(zeta[:, 0:L, 0:Mp] + zeta[:, 1:Lp, 0:Mp])
+            
+            z = np.empty((zetau.shape[0], N+use_w, hu.shape[0], hu.shape[1]))
+            z.fill(np.nan)
+            
+            #compute depth
+            for k in range(z.shape[0]):
+                z0 = (s[k]-C[k])*romsvars['hc'] + C[k]*hu
+                
+                for i in range(zetau.shape[0]) :
+                    z[i, k, :, :] = z0 + zetau[i, :, :]*(1.0 + z0/hu)
+            
+        elif point_type in [4, 'v']:
+            #averaging
+            hv = 0.5*(h[0:Lp, 0:M] + h[0:Lp, 1:Mp])
+            zetav = 0.5*(zeta[:, 0:Lp, 0:M] + zeta[:, 0:Lp, 1:Mp])
+            
+            z = np.empty((zetav.shape[0], N+use_w, hv.shape[0], hv.shape[1]))
+            z.fill(np.nan)
+            
+            #compute depth
+            for k in range(z.shape[0]):
+                z0 = (s[k]-C[k])*romsvars['hc'] + C[k]*hv
+                
+                for i in range(zetav.shape[0]) :
+                    z[i, k, :, :] = z0 + zetav[i, :,:]*(1.0 + z0/hv)
+            
+        elif use_w:
+            hr = h
+            zetar = zeta
+            
+            z = np.empty((zetar.shape[0], N+use_w, h.shape[0], h.shape[1]))
+            z.fill(np.nan)
+            
+            z[0, :, :] = -hr
+            for k in range(1, z.shape[0]):
+                z0 = (s[k]-C[k])*romsvars['hc'] + C[k]*hr
+                
+                for i in range(zetar.shape[0]) :
+                    z[i, k, :, :] = z0 + zetar[i, :, :]*(1.0 + z0/hr)
+                    
+        else:
+            raise RuntimeError('Invalid point_type "{}".'.format(point_type))
+    else:
+        raise NotImplementedError('Vstretching > 1 not supported yet.')
+        
+    return z
 
 def _stretching(romsvars=None, romsfile=None, use_w=False):
     '''
